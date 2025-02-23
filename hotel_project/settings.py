db0ad6f3bdf92c5f99add056e79e3f13e8bfffcb
dev-loper-72 <loperdev72@gmail.com>
2025-02-23 17:40:21 +0000

    more css

--- hotel_project/settings.py
+++ hotel_project/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,7 +129,6 @@ STATIC_URL = '/static/'
 # Check and set this during deployment
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
