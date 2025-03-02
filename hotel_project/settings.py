8e1fb2d56d213083eea115f375f9f10a51d9f2d6
dev-loper-72 <loperdev72@gmail.com>
2025-03-02 19:40:46 +0000

    test

--- hotel_project/settings.py
+++ hotel_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 
 
 from django.conf.global_settings import STATICFILES_DIRS
@@ -23,8 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l^c5r40e#f&r(2ddftc&5@1mymw!5hvqd307+fk4m44ql1&h11'
+env = environ.Env()
+environ.Env.read_env()  # Reads from .env file
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
